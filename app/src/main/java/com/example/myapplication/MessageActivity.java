f4491dfe04ebd71763b6f6e6411bb9b59f764c1a
1pdinesh97@gmail.com <1pdinesh97@gmail.com>
2019-10-31 05:06:13 +0800

    haven't finish

--- app/src/main/java/com/example/myapplication/MessageActivity.java
+++ app/src/main/java/com/example/myapplication/MessageActivity.java
@@ -12,11 +12,17 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.InputType;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -25,9 +31,11 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -36,7 +44,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -46,12 +59,16 @@ public class MessageActivity extends AppCompatActivity {
     private EditText inputMessage;
     MessageAdapter messageAdapter;
     List<SendReceiveMessage>mChat;
-    private ImageButton send;
+    private ImageButton send,voice;
+    private MediaRecorder mrecorder;
+    private String audioName=null;
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
     private Toolbar toolbar;
     private RecyclerView recyclerView;
+    private static final String LOG_TAG = "AudioRecord";
     DatabaseReference reference, reference1, reference2;
+    StorageReference  audio;
     ArrayList<String> myArrayList = new ArrayList<>();
     ArrayAdapter<String> arrayAdapter;
 
@@ -66,6 +83,14 @@ public class MessageActivity extends AppCompatActivity {
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
         inputMessage = findViewById(R.id.inputMessage);
+
+    //voice
+        voice=findViewById(R.id.mic);
+        audioName= Environment.getExternalStorageDirectory().getAbsolutePath();
+        audioName +="/recorded_audio.3gp";
+        audio= FirebaseStorage.getInstance().getReference();
+   //voice//
+
         send = findViewById(R.id.send);
         recyclerView = findViewById(R.id.messageList);
         recyclerView.setHasFixedSize(true);
@@ -93,17 +118,61 @@ public class MessageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String msg = inputMessage.getText().toString();
+
                 if(!msg.equals(""))
                 {
+                    voice.setVisibility(View.INVISIBLE);
                     sendMessage(firebaseUser.getUid(), userid, msg);
+
                 }
                 else
                 {
+                    voice.setVisibility(View.VISIBLE);
                     Toast.makeText(MessageActivity.this, "You can't send empty message!",Toast.LENGTH_SHORT).show();
                 }
                 inputMessage.setText("");
             }
         });
+
+        //voice//
+
+        voice.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)v.getLayoutParams();
+                int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 35, getResources().getDisplayMetrics());
+                int width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 140, getResources().getDisplayMetrics());
+
+                if(event.getAction()== MotionEvent.ACTION_DOWN){
+                    layoutParams.width = width + 5;
+                    layoutParams.height = height + 5;
+                    voice.setLayoutParams(layoutParams);
+                    voice.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
+                    startRecording();
+
+
+                }
+               else if(event.getAction() == MotionEvent.ACTION_UP)
+                {
+
+                    layoutParams.width = width ;
+                    layoutParams.height = height;
+                    voice.setLayoutParams(layoutParams);
+                    stopRecording();
+
+
+                }
+
+
+
+                return false;
+            }
+        });
+//voice//
+
+
 
         reference = FirebaseDatabase.getInstance().getReference("People").child(userid);
         reference.addValueEventListener(new ValueEventListener() {
@@ -119,6 +188,47 @@ public class MessageActivity extends AppCompatActivity {
         });
 
     }
+//voice
+    private void startRecording() {
+        mrecorder = new MediaRecorder();
+        mrecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mrecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mrecorder.setOutputFile(audioName);
+        mrecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mrecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mrecorder.start();
+    }
+
+
+    private void stopRecording() {
+        mrecorder.stop();
+        mrecorder.release();
+        mrecorder=null;
+
+
+        uploadaudio();
+
+    }
+
+    private void uploadaudio() {
+
+        StorageReference filepath =audio.child("audio").child("audio_new"+System.currentTimeMillis() +"audio.3gp");
+        Uri uri=Uri.fromFile(new File(audioName));
+
+        filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getApplicationContext(),"audio recorded",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+//voice
 
     @Override
     public boolean onCreateOptionsMenu(Menu edit) {
